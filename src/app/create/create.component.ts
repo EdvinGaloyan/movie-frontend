20ec850adee105f67ec85fcb698435c65dd42760
Edvin <edvin.galoyan@gmail.com>
2022-07-31 18:26:26 +0400

    Revert "s"
    This reverts commit 34eb501b82da671f5cc5050f18be55ed019ddd4d.

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -8,7 +8,7 @@ import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
-})fsdfsdfsdfsdf
+})
 export class CreateComponent implements OnInit {
 
   form: FormGroup;
